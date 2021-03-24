eca3cb5fc84e1e309495b1ced09a99c2b5633025
Jorge Lisboa <jorgelisboa659@gmail.com>
2021-03-23 23:34:53 -0300

    Update treino com C# e Mysql

---
+++ ClientAppWebService/ClientAppWebService/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ClientAppWebService
+{
+    public partial class Form1 : Form
+    {
+        localhost.WebService1 service = new localhost.WebService1();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String Coutries_Json = service.Countries();
+            DataTable Countries = JsonConvert.DeserializeObject<DataTable>(Coutries_Json); //Desserializando o arquivo que era JSON
+            dataGridView1.DataSource = Countries;
+        }
+    }
+}
---
+++ WebserviceTreino/WebserviceTreino/WebService1.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Newtonsoft.Json;
+
+namespace WebserviceTreino
+{
+    /// <summary>
+    /// Descrição resumida de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        DataTable dataCountries = new DataTable();
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Olá, Mundo";
+        }
+
+        [WebMethod]
+        public int Sum(int a, int b)
+        {
+            return a+b;
+        }
+
+        [WebMethod]
+        public string Countries()
+        {
+            //Minhas colunas
+            dataCountries.Columns.Add("Country Name");
+            dataCountries.Columns.Add("Continent");
+            //Adicionando minhas linhas (Country name, Continent)
+            dataCountries.Rows.Add("Brazil", "America");
+            dataCountries.Rows.Add("India", "Asia");
+            dataCountries.Rows.Add("USA", "America");
+            dataCountries.Rows.Add("Pakistan", "Asia");
+            dataCountries.Rows.Add("Germany", "Europe");
+
+            return JsonConvert.SerializeObject(dataCountries);
+        }
+    }
+}
